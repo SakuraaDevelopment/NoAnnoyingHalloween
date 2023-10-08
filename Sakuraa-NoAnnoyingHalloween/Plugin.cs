9557b3ea9dad6ca09eaa8b111526b02bdae5157d
SakuraaGT <huddleonpc@gmail.com>
2023-10-07 22:59:29 -0700

    Release

---
+++ Sakuraa-NoAnnoyingHalloween/Plugin.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+using GorillaTagScripts;
+
+namespace Sakuraa_NoAnnoyingHalloween
+{
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+		bool Enabled;
+        private GameObject lightningManager = null;
+		private GameObject ghostLurker = null;
+
+        void Start()
+		{
+            lightningManager = GameObject.Find("Environment Objects/PersistentObjects_Prefab/Halloween2023_PersistentObjects/LightningManager");
+			ghostLurker = GameObject.Find("Environment Objects/PersistentObjects_Prefab/Halloween2023_PersistentObjects/GhostLurker_Prefab");
+            Enabled = true;
+			Debug.Log("Sakuraas Lightning Disabler started!");
+        }
+
+		void OnEnable()
+		{
+			Enabled = true;
+			if (inRoom && lightningManager != null)
+			{
+				lightningManager.SetActive(false);
+				ghostLurker.SetActive(false);
+            }
+		}
+
+		void OnDisable()
+		{
+			Enabled = false;
+            lightningManager.SetActive(true);
+			ghostLurker.SetActive(true);
+        }
+
+		void Update()
+		{
+            if (lightningManager != null && inRoom && Enabled)
+            {
+                lightningManager.SetActive(false);
+				ghostLurker.SetActive(false);
+            }
+			else
+			{
+				lightningManager.SetActive(true);
+				ghostLurker.SetActive(true);
+			}
+        }
+
+		[ModdedGamemodeJoin]
+		public void OnJoin(string gamemode)
+		{
+			inRoom = true;
+		}
+
+		[ModdedGamemodeLeave]
+		public void OnLeave(string gamemode)
+		{
+			inRoom = false;
+		}
+	}
+}
---
+++ Sakuraa-NoAnnoyingHalloween/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Sakuraa_NoAnnoyingHalloween
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.sakuraa.gorillatag.sakuraa-noannoyinghalloween";
+		public const string Name = "Sakuraa_NoAnnoyingHalloween";
+		public const string Version = "1.0.0";
+	}
+}
